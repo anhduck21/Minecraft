915731b879b36b0db00fe9f196728a21aa903366
anhduck21 <ducanhdang2904@gmail.com>
2025-12-10 11:03:34 +0700

    Initial commit

---
+++ MinecraftAPI/MinecraftDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MinecraftAPI.Models; // đúng với namespace trong Resources.cs
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Data
+{
+    public class MinecraftDbContext : DbContext
+    {
+        public MinecraftDbContext(DbContextOptions<MinecraftDbContext> options)
+            : base(options) { }
+
+        // Bảng Resources
+        public DbSet<Resources> Resources { get; set; }
+
+        
+    }
+}
---
+++ Models/Resources.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinecraftAPI.Models
+{
+    public class Resources
+    {
+        [Key]
+        public int? ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? ResourceName { get; set; }
+
+        [MaxLength(50)]
+        public string? Rarity { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+    }
+}
