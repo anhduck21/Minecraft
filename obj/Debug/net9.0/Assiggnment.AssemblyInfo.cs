ce7d8e0746fa51c4acc62ea446493764c09b85c4
anhduck21 <ducanhdang2904@gmail.com>
2025-12-10 13:58:17 +0700

    Remove old Assiggnment solution and csproj, keep MinecraftAPI.csproj

--- obj/Debug/net9.0/Assiggnment.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assiggnment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assiggnment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b018cf8e3d33ded9f257cdbf69f99ce55cd98049")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assiggnment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assiggnment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
